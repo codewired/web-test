7da14f7b035b7f8716ba2a4d2a5259f765f519f5
codewired <chinedu@codewiredinc.com>
2023-02-10 12:22:03 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useScript } from './hooks/useScript';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
 
 function App() {
+  useScript("https://interview-slow-frontend.vercel.synthesis.is/api/messaging");
+  const [inputs, setInputs] = useState({});
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    window.Synthesis.event("Wait List", inputs.email)
+    console.log(`The Full names are : ${inputs.fullname}`)
+  }
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value
+
+    setInputs(values => ({...values, [name]: value}))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="NameForm">
+      <form onSubmit={handleSubmit}>
+        <Form.Group size="lg">
+          <Form.Label> Enter Email Address: </Form.Label>
+          <Form.Control type="text" name="email" value={inputs.email || ""} onChange={handleChange} />
+        </Form.Group>
+        <Form.Group size="lg">
+          <Form.Label> Enter Full Names: </Form.Label>
+          <Form.Control type="text" name="fullname" value={inputs.fullname || ""} onChange={handleChange} />       
+        </Form.Group>
+        <Button type="submit" size="lg">
+          Send Info
+        </Button>
+      </form>
     </div>
   );
 }
---
+++ src/hooks/useScript.js
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+
+export const useScript = url => {
+    useEffect(() => {
+        const script = document.createElement('script');
+
+        script.src = url;
+        script.async = false;
+
+        document.body.appendChild(script);
+        return () => {
+            document.body.removeChild(script)
+        }
+    }, [url]);
+}
